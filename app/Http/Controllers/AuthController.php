b9bdaf1a11f236ee82bc04f90d2d38a44aaa0ecc
jevonkagenou <kichironomercy@gmail.com>
2023-08-10 15:40:39 +0700

    Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,27 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
 
 class AuthController extends Controller
 {
     public function login(){
-        return view('login');
+        return view('loginRegister\Login');
     }
 
     public function loginuser(Request $request){
         $credentials = $request->only('email', 'password');
-
+        // dd($credentials);
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
 
-            // Gunakan fungsi hasRole untuk memeriksa role pengguna
             if ($user->hasRole('admin')) {
-                return redirect()->route('');
+                return redirect()->back()->with('success', 'Login Admin Berhasil');
             } elseif ($user->hasRole('user')) {
-                return redirect()->route('');
+                return redirect()->back()->with('success', 'Login User Berhasil');
             }
         }
-        return redirect()->route('login')->with('error', 'Email Atau Password Yang Anda Masukkan Salah');
+        return redirect()->back()->with('error', 'Email Atau Password Yang Anda Masukkan Salah');
     }
 
     public function logout(Request $request)
--- routes/felix.php
+++ routes/felix.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\AuthController;
 |
 */
 Route::get('/login',[AuthController::class,'login'])->name('login');
-Route::post('/loginuser',[AuthController::class,'loginuser'])->name('Loginuser');
+Route::post('/loginuser',[AuthController::class,'loginuser'])->name('loginuser');
 Route::get('/', function () {
         return view('welcome');
 });
