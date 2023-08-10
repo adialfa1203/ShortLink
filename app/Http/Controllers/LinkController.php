328b9ea59e3d30f20765f0bb74f4ce94914e5309
Gmbs <gmbsstok@gmail.com>
2023-08-10 15:31:15 +0700

    udah

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LinkController extends Controller
+{
+    public function Link()
+    {
+        return view('Link');
+    }
+}
--- routes/gmbs.php
+++ routes/gmbs.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,10 +13,4 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-require __DIR__."/adi.php";
-require __DIR__."/gmbs.php";
+Route::get('/Link', [LinkController::class, 'Link'])->name('Link');
