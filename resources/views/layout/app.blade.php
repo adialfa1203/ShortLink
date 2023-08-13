85e532c6071d1e7372c8a77c5f044b51b38a5a87
Gmbs <gmbsstok@gmail.com>
2023-08-13 10:20:06 +0700

    halaman link

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -20,6 +20,7 @@
     <link id="fontsLink"
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&amp;display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- jsvectormap css -->
     <link href="{{ asset('template/themesbrand.com/steex/layouts/assets/libs/jsvectormap/css/jsvectormap.min.css') }}" rel="stylesheet" type="text/css">
