1883724cbd14053ee03f73920ad42c5718568fd4
jihansaharani <saharanijihan1@gmail.com>
2023-08-24 11:24:25 +0700

    Header Footer 2

--- resources/views/HelpSupport/BillingSubscriptions.blade.php
+++ resources/views/HelpSupport/BillingSubscriptions.blade.php
@@ -109,7 +109,7 @@ header {
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    <a href="/HelpSupport" class="col-1 btn btn-info">
+                    <a href="/HelpSupport" class="col-1 btn btn-info btn-sm">
                         <i class="fas fa-arrow-left"></i> Kembali
                     </a>
                     <p></p>
@@ -170,20 +170,92 @@ header {
             </div>
         </div>
        </div>
-       <footer class="footer">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-6">
-                    <script>
-                        document.write(new Date().getFullYear())
-                    </script> © Link.id
-                </div>
-                <div class="col-6">
-                    <div class="text-sm-end d-none d-sm-block">
-                        Design & Develop by Link.id
+       <footer>
+        <div class="top_footer" id="kontak">
+            <div class="container">
+                <div class="row">
+                    <!-- Logo -->
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <div class="abt_side">
+                            <div class="logo">
+                                <img src="https://i.postimg.cc/QdZvjL3f/Logo-LINK-ID.png" alt="image"
+                                    style="margin-top: -27%;">
+                                    <ul style="margin-top: -16%; margin-left:20px;">
+                                        <li style="color: white; font-size:14px;">S.id adalah platform untuk orang-orang untuk menunjukkan keahlian mereka dalam membuat situs mikro dan memperpendek tautan terpendek dengan kode s.id/.</li>
+                                    </ul>
+                                </div>
+                        </div>
+                    </div>
+
+                    <!-- Footer Links -->
+                    
+                    <div class="col-lg-2 col-md-6 col-12">
+                        <div class="links">
+                            <h3>Dukungan</h3>
+                            <ul style="text-align: justify;">
+                                <li><a href="#kontak">Kebijakan Privasi</a></li>
+                                <li><a href="/Home">Bantuan</a></li>
+                                <li><a href="#features">Laporkan</a></li>
+                                <li><a href="#kontak">Status</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Contact Information -->
+                    <div class="col-lg-2 col-md-6 col-12">
+                        <div class="links">
+                            <h3>Hubungi Kami</h3>
+                            <ul style="text-align: justify;">
+                                <li>
+                                    <a href="https://wa.me/085606270454">
+                                        <i class="fab fa-whatsapp"></i>
+                                        085606270454
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#features">
+                                        <i class="fab fa-instagram"></i>
+                                        @link.id
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#features">
+                                        <i class="fab fa-twitter"></i>
+                                        @link.id
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Comment Form -->
+                    <div class="col-lg-5 col-md-6 col-12 mb-5" >
+                        <form action="javascript:void(0);" class="mt-3">
+                            <textarea class="form-control bg-light border-light" id="exampleFormControlTextarea1" rows="3"
+                                placeholder="Tambahkan Komentar"></textarea>
+                            <div class="text-end mt-2">
+                                <a href="javascript:void(0);" class="btn btn-success">Kirim</a>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
+        </div>
+
+        <!-- Bottom Footer -->
+        <div class="bottom_footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-right">
+                        <p>© LINK.ID Dikelola oleh PT. Hummatech</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Go Top Button -->
+        <div class="go_top">
+            <span><img src="https://i.postimg.cc/MZtYYpPg/go-top.png" alt="image"></span>
         </div>
     </footer>
         <!-- Footer-Section end -->
