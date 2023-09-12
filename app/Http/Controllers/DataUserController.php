3c218395d5b96c2446381ff6698a477a8f6f6ef1
jihansaharani <saharanijihan1@gmail.com>
2023-09-12 09:56:49 +0700

    count blokir

--- app/Http/Controllers/DataUserController.php
+++ app/Http/Controllers/DataUserController.php
@@ -19,7 +19,10 @@ class DataUserController extends Controller
 
         $totalVisits = ShortURLVisit::query()->count();
 
+        $totaldiblokir = User::where('is_banned', 1)->count();
+
         $users = User::where('email', '!=', 'admin@gmail.com')->get();
+        $totalUser -= 1;
         $count = [];
         foreach ($users as $user) {
             $count[$user->id] = ShortUrl::where('user_id', $user->id)->count();
@@ -27,7 +30,7 @@ class DataUserController extends Controller
 
         // Mengurutkan data berdasarkan jumlah pengunjung
         arsort($count);
-        return view('Admin.DataUserAdmin', compact('data','totalUser', 'totalUrl', 'totalVisits', 'users', 'count','totalMicrosite'));
+        return view('Admin.DataUserAdmin', compact('data','totalUser', 'totalUrl', 'totalVisits', 'users', 'count','totalMicrosite','totaldiblokir'));
     }
     
 
